daf8cf7630e827a1a5618455f101fd12e7712d3c
FirenzesEagle <liumingbo2008@gmail.com>
2016-03-17 16:00:13 +0800

    Fix a bug

--- src/cn/edu/bupt/Longest_Substring_Without_Repeating_Characters.java
+++ src/cn/edu/bupt/Longest_Substring_Without_Repeating_Characters.java
@@ -13,22 +13,24 @@ import java.util.HashMap;
 public class Longest_Substring_Without_Repeating_Characters {
 
     public static void main(String[] args) {
-        System.out.println(new Longest_Substring_Without_Repeating_Characters().lengthOfLongestSubstring("bbbbb"));
+        System.out.println(new Longest_Substring_Without_Repeating_Characters().lengthOfLongestSubstring("abcabcbb"));
     }
 
     public int lengthOfLongestSubstring(String s) {
         if (s.length() == 0) return 0;
-        //keep a hashmap which stores the characters in string as keys and their positions as values
+        //使用HashMap来存储字符串，Key存储单个字符，Value存储字符在字符串中所在的位置
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        int max = 0;
-        for (int i = 0, j = 0; i < s.length(); ++i) {
+        //left记录某字符串上次出现的位置
+        int max = 0, left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            //如果该字符已经存在
             if (map.containsKey(s.charAt(i))) {
-                //if found in hashmap that value,then update j i.e pointer to the right of the same character last found.
-                j = Math.max(j, map.get(s.charAt(i)) + 1);
+                //更新该字符上次出现的位置
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
             }
-            //else put in map and get max pointer update with the current longest string
+            //否则将该字符的位置作为Value放置到HashMap中，计算目前最长字符串
             map.put(s.charAt(i), i);
-            max = Math.max(max, i - j + 1);
+            max = Math.max(max, i - left + 1);
         }
         return max;
     }
