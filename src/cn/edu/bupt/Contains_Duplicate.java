f8c519f33f7b854117a33ff997f168b50a730f95
FirenzesEagle <liumingbo2008@gmail.com>
2016-11-30 12:05:56 +0800

    solve 217. Contains Duplicate

---
+++ src/cn/edu/bupt/Contains_Duplicate.java
@@ -0,0 +1,26 @@
+package cn.edu.bupt;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by FirenzesEagle on 2016/11/29.
+ * Email:liumingbo2008@gmail.com
+ */
+
+/*217. Contains Duplicate
+Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct..
+ */
+public class Contains_Duplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> integerSet = new HashSet<>();
+        for (int x : nums) {
+            integerSet.add(x);
+        }
+        if (integerSet.size() != nums.length)
+            return true;
+        else
+            return false;
+    }
+}
